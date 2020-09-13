61747e723407166026dfea104b3a611c4ad4b5b7
Matthew <admin@langomatisch.de>
2020-09-13 16:14:42 +0200

    abc update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function test() {
         setTimeout(() => {
             nextBot();
             test();
-        }, 100); // default 4000 on bukkit.yml
+        }, 3000); // default 4000 on bukkit.yml
     }
 }
 
@@ -20,8 +20,71 @@ function nextBot() {
     console.log(`#${index}`);
     let bot = initBot(index);
     bot.on('login', () => {
-        bot.chat('Heyho!');
+        //bot.chat('Wuff');
     })
+
+	timer();
+	
+	function timer(){
+		setTimeout(timer, 1);
+		if(bot.entity != undefined){
+			let toFollow = `${config["to-follow"]}`;
+			//var toFollow = "Block_Eater";
+			
+			var id, entity, dist;
+			var best = null;
+			var bestDistance = null;
+			if(bot != null){
+				for(id in bot.entities) {
+					entity = bot.entities[id];
+					//if(type && entity.type !== type) continue;
+					if(entity === bot.entity) continue;
+					dist = bot.entity.position.distanceTo(entity.position);
+					var totest = entity.position + '';
+					if(!best || dist < bestDistance) {
+						if(entity.username == toFollow){
+							best = entity;
+							bestDistance = dist;
+						}
+					}
+				}
+			}
+			
+			
+			
+			if(best != null && bot.entity != null){
+					if(bot.entity.position.distanceTo(best.position) < 1){
+							bot.setControlState('forward', false)
+					}else{
+						bot.setControlState('forward', true)
+					}
+				}
+				var getY = 0;
+				var getY2 = 0;
+				
+				if(best != null && bot.entity != null){
+				
+					var getPosition = best.position + '';
+					var getPositionSplit = getPosition.split(",");
+					getY = getPositionSplit[1];
+					
+					var getPosition2 = bot.entity.position + '';
+					var getPositionSplit2 = getPosition2.split(",");
+					getY2 = getPositionSplit2[1];
+				
+				}
+				
+				
+				if((getY-getY2	) > 0){
+					bot.setControlState('jump', true)
+				}else{
+					bot.setControlState('jump', false)
+				}
+				if(best != null){
+					bot.lookAt(best.position.offset(0, +1.5, 0));
+				}
+		}
+	}
 }
 
 function initBot(index) {
